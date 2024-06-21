7e6c7a000bcfb24e83b7486d4f2cb2979da7454c
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-06-21 22:23:15 +0800

    Update [YouTube] like and dislike buttons - padding patch.js
    bump version to 1.1
    
    add some code to make it compatible with Return YouTube Dislike addon

--- [YouTube] like and dislike buttons - padding patch.js
+++ [YouTube] like and dislike buttons - padding patch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [YouTube] like and dislike buttons - padding patch
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  simple patch to make the like and dislike buttons look "symmetrical" to each other, this is noticeable when you use the Return YouTube Dislike addon
 // @author       John Patrick Adem
 // @match        *://*.youtube.com/*
@@ -14,6 +14,7 @@
 // ==/UserScript==
 
 var waitTimeMs = 3000; // default wait time is 3 seconds (in milliseconds)
+var ReturnYouTubeDislikeCompatibility = false; // make this userscript compatible with Return YouTube Dislike - Extension / UserScript verrsions
 
 /*
 	style
@@ -36,7 +37,36 @@ var waitTimeMs = 3000; // default wait time is 3 seconds (in milliseconds)
 	MAIN
 */
 
+var likebuttonpx;
+var dislikebuttonpx;
+var newratiobarpx;
+
 setTimeout(function(){
 	document.querySelector("like-button-view-model > toggle-button-view-model > button-view-model > button").classList.add("like-and-dislike-padding-patch"); // like button
 	document.querySelector("dislike-button-view-model > toggle-button-view-model > button-view-model > button").classList.add("like-and-dislike-padding-patch"); // dislike button
 }, waitTimeMs);
+
+if (ReturnYouTubeDislikeCompatibility == true) {
+	setTimeout(function(){
+		// check if the ratio bar from "Return YouTube Dislike - Extension" exists in the HTML
+		if (document.querySelector(".ryd-tooltip.ryd-tooltip-new-design") != null) {
+			likebuttonpx = document.querySelector("like-button-view-model").clientWidth;
+			dislikebuttonpx = document.querySelector("dislike-button-view-model").clientWidth;
+			newratiobarpx = likebuttonpx + dislikebuttonpx;
+			
+			document.querySelector(".ryd-tooltip.ryd-tooltip-new-design").style = "width: " + newratiobarpx + "px";
+		}
+		
+		// check if the ratio bar from "Return YouTube Dislike - UserScript" exists in the HTML
+		if (document.querySelector(".ryd-tooltip") != null) {
+			likebuttonpx = document.querySelector("like-button-view-model").clientWidth;
+			dislikebuttonpx = document.querySelector("dislike-button-view-model").clientWidth;
+			newratiobarpx = likebuttonpx + dislikebuttonpx;
+			
+			document.querySelector(".ryd-tooltip").style = "width: " + newratiobarpx + "px";
+		}
+		
+		// this also patches the padding-bottom of the menu buttons container
+		document.querySelector("#top-row").style = "border-bottom: 1px solid var(--yt-spec-10-percent-layer); padding-bottom: 6px;"; // originally, this was 10px, making the ratio bar appear to be "floating" away
+	}, waitTimeMs);
+}
