4e1ade5cfd5f82ec76a3a23c8912d8c4644f3329
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-07-29 20:12:12 +0800

    Add files via upload

---
+++ [YouTube] y2mate.is downloader - download button overrider.js
@@ -0,0 +1,55 @@
+﻿// ==UserScript==
+// @name         [YouTube] y2mate,is downloader - download button overrider
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  simple userscript to override the native download button to download videos from y2mate.is
+// @author       John Patrick Adem
+// @match        *://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @downloadURL  https://github.com/jpa102/ytscripts/raw/main/%5BYouTube%5D%20Hide%20the%20Download%20button.js
+// @updateURL    https://github.com/jpa102/ytscripts/raw/main/%5BYouTube%5D%20Hide%20the%20Download%20button.js
+// @connect      youtube.com
+// @run-at       document-end
+// ==/UserScript==
+
+// script settings 「スクリプトの設定」
+var waitTimeMs = 5500; // the wait time for this userscript to load, default is 5.5 seconds (5500 milliseconds)
+var DownloaderSite = "https://www.youtubepi.com/watch?v="; // redirects to y2mate.is, this is stated as a tip to quickly download from their site
+
+
+
+// ====================== [MAIN] ======================
+// you may notice the console.log() codes are commented, uncomment them for debugging
+
+// code credit to https://www.github.com/Anarios/return-youtube-dislike
+function getVideoId() {
+	const urlObject = new URL(window.location.href);
+	const pathname = urlObject.pathname;
+	if (pathname.startsWith("/clip")) {
+		return (document.querySelector("meta[itemprop='videoId']") || document.querySelector("meta[itemprop='identifier']")).content;
+	} else {
+		if (pathname.startsWith("/shorts")) {
+			return pathname.slice(8);
+		}
+		return urlObject.searchParams.get("v");
+	}
+}
+
+function downloadButton() {
+	window.open("https://www.youtubepi.com/watch?v=" + getVideoId());
+	
+	setTimeout(function() {
+		if (document.querySelector("ytd-download-button-renderer") != null && document.querySelector("ytd-download-button-renderer").hasAttribute("is-hidden") == true) {
+			// console.log("download button is hidden, trying to unhide...");
+			document.querySelector("ytd-download-button-renderer").removeAttribute("is-hidden");
+		} else {
+			// console.log("[ERROR] the download button has been removed!");
+			return;
+		}
+	}, 600);
+}
+
+setTimeout(function() {
+	document.querySelector("ytd-download-button-renderer > ytd-button-renderer").addEventListener("click", downloadButton);
+	// console.log("[SUCCESS] overriden the download button! now you can download the current video...");
+}, waitTimeMs);
