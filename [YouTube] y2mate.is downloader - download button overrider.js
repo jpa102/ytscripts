931413cb4f837d63de396fabd1776dde8fa5d430
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-07-30 09:53:51 +0800

    [YouTube] y2mate.is downloader - download button overrider.js の更新
    add a conditional and option to display a popup if a user wants to download the video from y2mate.is or in the youtube website client itself

--- [YouTube] y2mate.is downloader - download button overrider.js
+++ [YouTube] y2mate.is downloader - download button overrider.js
@@ -14,6 +14,7 @@
 
 // script settings 「スクリプトの設定」
 var waitTimeMs = 5500; // the wait time for this userscript to load, default is 5.5 seconds (5500 milliseconds)
+var DisplayConsentOption = false; // show a dialog whether you want to download the video to y2mate.is or in the youtube site (there's an option for offline playback in itself)
 var DownloaderSite = "https://www.youtubepi.com/watch?v="; // redirects to y2mate.is, this is stated as a tip to quickly download from their site
 var oldDownloadButton = false; // if it doesn't work, setting this to true checks if the download button comes from a "legacy" version of the watch page (old html elements)
 
@@ -21,6 +22,16 @@ var oldDownloadButton = false; // if it doesn't work, setting this to true check
 
 // ====================== [MAIN] ======================
 // you may notice the console.log() codes are commented, uncomment them for debugging
+
+// message strings
+var DownloaderSiteName = "y2mate.is";
+var DownloadConfirmMain = "Do you want to download this video from " + DownloaderSiteName +"?\n\nPress OK if you want to continue, press Cancel to download it offline to your YouTube website client.";
+var DebugMessage01 = "[SUCCESS] overriden the download button!";
+var DebugMessage02 = "download button is hidden, trying to unhide...";
+var DebugMessage03 = "[ERROR] the download button has been removed!";
+var DebugMessage04 = "[INFO] the user pressed the OK button.";
+var DebugMessage05 = "[INFO] the user pressed the Cancel button.";
+
 
 // code credit to https://www.github.com/Anarios/return-youtube-dislike
 function getVideoId() {
@@ -37,17 +48,37 @@ function getVideoId() {
 }
 
 function downloadButton() {
-	window.open("https://www.youtubepi.com/watch?v=" + getVideoId());
-	
-	setTimeout(function() {
-		if (document.querySelector("ytd-download-button-renderer") != null && document.querySelector("ytd-download-button-renderer").hasAttribute("is-hidden") == true) {
-			// console.log("download button is hidden, trying to unhide...");
-			document.querySelector("ytd-download-button-renderer").removeAttribute("is-hidden");
+	if (DisplayConsentOption == true) {
+		if (window.confirm(DownloadConfirmMain) == true) {
+			window.open("https://www.youtubepi.com/watch?v=" + getVideoId());
+			
+			setTimeout(function() {
+				if (document.querySelector("ytd-download-button-renderer") != null && document.querySelector("ytd-download-button-renderer").hasAttribute("is-hidden") == true) {
+					// console.log(DebugMessage02);
+					document.querySelector("ytd-download-button-renderer").removeAttribute("is-hidden");
+				} else {
+					// console.log(DebugMessage03);
+					return;
+				}
+			}, 600);
+			
+			// console.log(DebugMessage04);
 		} else {
-			// console.log("[ERROR] the download button has been removed!");
-			return;
+			// console.log(DebugMessage05);
 		}
-	}, 600);
+	} else {
+		window.open("https://www.youtubepi.com/watch?v=" + getVideoId());
+		
+		setTimeout(function() {
+			if (document.querySelector("ytd-download-button-renderer") != null && document.querySelector("ytd-download-button-renderer").hasAttribute("is-hidden") == true) {
+				// console.log(DebugMessage02);
+				document.querySelector("ytd-download-button-renderer").removeAttribute("is-hidden");
+			} else {
+				// console.log(DebugMessage03);
+				return;
+			}
+		}, 600);
+	}
 }
 
 setTimeout(function() {
@@ -58,10 +89,10 @@ setTimeout(function() {
 		}
 	} else {
 		if (document.querySelector("ytd-download-button-renderer > ytd-button-renderer > yt-button-shape > button").hasAttribute("disabled") == true) {
-			document.querySelector("ytd-download-button-renderer > ytd-button-renderer > yt-button-shape > button").removeAttribute("disabled");
+			document.querySelector("ytd-download-button-renderer > ytd-button-renderer > yt-button-shape  > button").removeAttribute("disabled");
 		}
 	}
 	
 	document.querySelector("ytd-download-button-renderer > ytd-button-renderer").addEventListener("click", downloadButton);
-	// console.log("[SUCCESS] overriden the download button! now you can download the current video...");
+	// console.log(DebugMessage01);
 }, waitTimeMs);
