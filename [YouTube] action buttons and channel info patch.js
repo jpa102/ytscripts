d39978ca309cf869123b72613f9e2a6279f629ef
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-09-01 03:42:30 +0800

    [YouTube] action buttons and channel info patch.js の更新
    add a require line to make this script work again (youtube website is now becoming more restrictive)

--- [YouTube] action buttons and channel info patch.js
+++ [YouTube] action buttons and channel info patch.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @downloadURL  https://github.com/jpa102/ytscripts/raw/main/%5BYouTube%5D%20action%20buttons%20and%20channel%20info%20patch.js
 // @updateURL    https://github.com/jpa102/ytscripts/raw/main/%5BYouTube%5D%20action%20buttons%20and%20channel%20info%20patch.js
+// @require      https://raw.githubusercontent.com/jpa102/ytscripts/main/trustedtypes_patch.js
 // @connect      youtube.com
 // @run-at       document-start
 // ==/UserScript==
